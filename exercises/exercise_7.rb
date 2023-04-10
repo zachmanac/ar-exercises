f2bf8f956d3c20db95234d1f073201377ea3affc
zachmanac <zachstephenson94@gmail.com>
2023-04-10 10:33:02 -0700

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store_id, numericality: {only_integer: true}, presence: true
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :mens_or_womens
+
+  def mens_or_womens
+    if mens_apparel == nil && womens_apparel == nil || mens_apparel == false && womens_apparel == false
+    errors.add(:base, "must have at least one of mens_apparel or womens_apparel")
+    end
+  end
+
+end
+
+storeName = "Tofino"
+Store.create({name: "Port Hardy", annual_revenue: 350000, mens_apparel: true, womens_apparel: false})
+p Store.create({name: storeName}).errors.full_messages
